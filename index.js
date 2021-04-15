c883cfb6edcae00cb7f471a901c071bc34a745af
Matt Brown <matthewtbrown124@gmail.com>
2021-04-15 16:09:44 -0500

    commit tree 2a22a6e794e4a88b73c5b259eeb6a9ba2a81e551

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let formData = {
+      name: userName,
+      email: userEmail
+    };
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+  
+      return fetch("http://localhost:3000/users", configObj)
+          .then(function (response) {
+              return response.json();
+          })
+          .then((object) => appendToTheDom(object))
+      .catch((error) => document.body.innerHTML = error.message);
+  }
+  
+  function appendToTheDom(object) {
+      document.body.innerHTML = object.id ;
+  }
